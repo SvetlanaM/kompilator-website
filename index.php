cac89f5ed6045506cce58cceeef931c09d4a0353
SvetlanaM <richard.margeta@gmail.com>
2020-12-08 12:01:41 +0100

    Optimalisation of query and css files

--- index.php
+++ index.php
@@ -341,8 +341,10 @@
         </div>
     </section>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+
+    <script src="vendor/jquery/jquery.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="vendor/jquery/popper.min.js"></script>
     <?php include "footer.html" ?>
 </body>
 
